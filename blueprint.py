af9e2cc922c5da4f9375d79b3cf8d96611eb81fa
siddh30 <38179155+siddh30@users.noreply.github.com>
2018-11-10 04:44:09 +0530

    Model Of This Project
    This is the model of this project . I will be using Tkinter for implementing the Graphical User Interface for this question. The basic agenda of this project is to create an interactive python game in which the user will have to guess the word based on a series of questions.

---
+++ blueprint.py
@@ -0,0 +1,62 @@
+# Model Of the project
+
+# import for the GUI
+try:
+    from tnkinter import *
+
+MAX = 10
+MIN = 1
+
+#bluepruint of the logic of the code
+
+class Application(Frame):
+    #The Gui of the application
+
+    def __init__(self) :
+        return
+
+
+    def create_widgets(self):
+        return
+
+
+    def get_guess(self):
+        return
+
+    def check_guess(self,guess):
+        return
+
+
+    def display_message(self ,message) :
+        return
+
+
+    def reset(self):
+        return
+
+    def resetgame(self):
+        return
+
+
+    def main(self):
+        # to run the program
+        return
+
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
